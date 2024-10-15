b7bc97111a53657a8e0ead440f0c1f17e9f0cb5e
Seraphin <149839735+KawanSerafim@users.noreply.github.com>
2024-10-15 16:35:09 -0300

    Update Aula10_Casa_Ex_Constraints2.sql - Tabelas criadas

--- SQL/Casa/Aula10_Casa_Ex_Constraints2.sql
+++ SQL/Casa/Aula10_Casa_Ex_Constraints2.sql
@@ -28,7 +28,76 @@ Ano_Modelo			INT					NOT NULL,
 Data_Aquisicao		DATE				NOT NULL,
 ClienteID			INT					NOT NULL
 
+PRIMARY KEY(Placa)
 FOREIGN KEY(ClienteID) REFERENCES Cliente(ID)
+
+)
+GO
+
+CREATE TABLE Telefone_Cliente (
+
+	ClienteID			INT					NOT NULL,
+	Telefone			VARCHAR(11)			NOT NULL
+
+	PRIMARY KEY(Telefone)
+	FOREIGN KEY(ClienteID) REFERENCES Cliente(ID)
+
+)
+GO
+
+CREATE TABLE Peca (
+
+ID				INT						NOT NULL,
+Nome			VARCHAR(30)				NOT NULL,
+Preco			DECIMAL(4, 2)			NOT NULL,
+Estoque			INT						NOT NULL
+
+PRIMARY KEY(ID)
+
+)
+GO
+
+CREATE TABLE Categoria (
+
+ID					INT						NOT NULL,
+Categoria			VARCHAR(10)				NOT NULL,
+Valor_Hora			DECIMAL(4, 2)			NOT NULL
+
+PRIMARY KEY(ID)
+
+)
+GO
+
+CREATE TABLE Funcionario (
+
+ID								INT						NOT NULL,
+Nome							VARCHAR(100)			NOT NULL,
+Logradouro						VARCHAR(200)			NOT NULL,
+Numero							INT						NOT NULL,
+Telefone						CHAR(11)				NOT NULL,
+Categoria_Habilitacao			VARCHAR(2)				NOT NULL,
+CategoriaID						INT						NOT NULL
+
+PRIMARY KEY(ID)
+FOREIGN KEY(CategoriaID) REFERENCES Categoria(ID)
+
+)
+GO
+
+CREATE TABLE Reparo (
+
+	VeiculoPlaca			CHAR(7)					NOT NULL,
+	FuncionarioID			INT						NOT NULL,
+	PecaID					INT						NOT NULL,
+	Data_Reparo				DATE					NOT NULL,
+	Custo_Total				DECIMAL(4, 2)			NOT NULL,
+	Tempo					INTEGER					NOT NULL
+
+	PRIMARY KEY(Data_Reparo, VeiculoPlaca, FuncionarioID, PecaID)
+	
+	FOREIGN KEY(VeiculoPlaca) REFERENCES Veiculo(Placa),
+	FOREIGN KEY(FuncionarioID) REFERENCES Funcionario(ID),
+	FOREIGN KEY(PecaID) REFERENCES Peca(ID)
 
 )
 GO

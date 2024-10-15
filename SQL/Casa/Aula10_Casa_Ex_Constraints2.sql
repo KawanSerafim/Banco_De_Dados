2ac575d123223e9e3fea13bd78c83858a7195bf2
Seraphin <149839735+KawanSerafim@users.noreply.github.com>
2024-10-15 18:04:59 -0300

    Update Aula10_Casa_Ex_Constraints2.sql - Exercício finalizado.

--- SQL/Casa/Aula10_Casa_Ex_Constraints2.sql
+++ SQL/Casa/Aula10_Casa_Ex_Constraints2.sql
@@ -5,11 +5,11 @@ GO
 
 CREATE TABLE Cliente (
 
-ID				INT					NOT NULL,
+ID				INT					NOT NULL		IDENTITY(3401, 15),
 Nome			VARCHAR(100)		NOT NULL,
 Logradouro		VARCHAR(200)		NOT NULL,
-Numero			INT					NOT NULL,
-CEP				CHAR(8)				NOT NULL,
+Numero			INT					NOT NULL		CHECK(Numero >= 0),
+CEP				CHAR(8)				NOT NULL		CHECK(LEN(CEP) = 8),
 Complemento		VARCHAR(255)		NOT NULL
 
 PRIMARY KEY(ID)
@@ -19,17 +19,23 @@ GO
 
 CREATE TABLE Veiculo (
 
-Placa				CHAR(7)				NOT NULL,
+Placa				CHAR(7)				NOT NULL		CHECK(LEN(Placa) = 7),
 Marca				VARCHAR(30)			NOT NULL,
 Modelo				VARCHAR(30)			NOT NULL,
 Cor					VARCHAR(15)			NOT NULL,
-Ano_Fabricacao		INT					NOT NULL,
+Ano_Fabricacao		INT					NOT NULL		CHECK((Ano_Fabricacao > 1997)),
 Ano_Modelo			INT					NOT NULL,
 Data_Aquisicao		DATE				NOT NULL,
 ClienteID			INT					NOT NULL
 
-PRIMARY KEY(Placa)
-FOREIGN KEY(ClienteID) REFERENCES Cliente(ID)
+PRIMARY KEY(Placa),
+FOREIGN KEY(ClienteID) REFERENCES Cliente(ID),
+
+CONSTRAINT Ano_Modelo_Fabricacao
+
+	CHECK((Ano_Modelo > 1997) AND 
+		  (Ano_Modelo = Ano_Fabricacao) AND 
+		  (Ano_Modelo = Ano_Fabricacao + 1)),
 
 )
 GO
@@ -37,7 +43,7 @@ GO
 CREATE TABLE Telefone_Cliente (
 
 	ClienteID			INT					NOT NULL,
-	Telefone			VARCHAR(11)			NOT NULL
+	Telefone			VARCHAR(11)			NOT NULL		CHECK((LEN(Telefone) >= 10) OR (LEN(Telefone) = 11))
 
 	PRIMARY KEY(Telefone)
 	FOREIGN KEY(ClienteID) REFERENCES Cliente(ID)
@@ -47,10 +53,10 @@ GO
 
 CREATE TABLE Peca (
 
-ID				INT						NOT NULL,
-Nome			VARCHAR(30)				NOT NULL,
-Preco			DECIMAL(4, 2)			NOT NULL,
-Estoque			INT						NOT NULL
+ID				INT						NOT NULL		IDENTITY(3411, 7),
+Nome			VARCHAR(30)				NOT NULL		UNIQUE,
+Preco			DECIMAL(4, 2)			NOT NULL		CHECK(Preco >= 0.00),
+Estoque			INT						NOT NULL		CHECK(Estoque >= 10)
 
 PRIMARY KEY(ID)
 
@@ -59,23 +65,38 @@ GO
 
 CREATE TABLE Categoria (
 
-ID					INT						NOT NULL,
-Categoria			VARCHAR(10)				NOT NULL,
+ID					INT						NOT NULL		IDENTITY(1, 1),
+Categoria			VARCHAR(10)				NOT NULL		CHECK(UPPER(Categoria) = 'ESTAGIÁRIO' OR
+																  UPPER(Categoria) = 'NÍVEL 1' OR
+																  UPPER(Categoria) = 'NÍVEL 2' OR
+																  UPPER(Categoria) = 'NÍVEL 3'),
 Valor_Hora			DECIMAL(4, 2)			NOT NULL
 
-PRIMARY KEY(ID)
+PRIMARY KEY(ID),
+
+CONSTRAINT Categoria_Valor_Hora
+
+	CHECK((
+			((Categoria = 'ESTAGIÁRIO') AND (Valor_Hora > 15.00)) OR
+			((Categoria = 'NÍVEL 1') AND (Valor_Hora > 35.00)) OR
+			((Categoria = 'NÍVEL 2') AND (Valor_Hora > 50.00))
+			))
 
 )
 GO
 
 CREATE TABLE Funcionario (
 
-ID								INT						NOT NULL,
+ID								INT						NOT NULL		IDENTITY(101, 1),
 Nome							VARCHAR(100)			NOT NULL,
 Logradouro						VARCHAR(200)			NOT NULL,
-Numero							INT						NOT NULL,
-Telefone						CHAR(11)				NOT NULL,
-Categoria_Habilitacao			VARCHAR(2)				NOT NULL,
+Numero							INT						NOT NULL		CHECK(Numero >= 0),
+Telefone						CHAR(11)				NOT NULL		CHECK((LEN(Telefone) >= 10) OR (LEN(Telefone) = 11)),
+Categoria_Habilitacao			VARCHAR(2)				NOT NULL		CHECK(UPPER(Categoria_Habilitacao) = 'A' AND
+																			  UPPER(Categoria_Habilitacao) = 'B' AND
+																			  UPPER(Categoria_Habilitacao) = 'C' AND
+																			  UPPER(Categoria_Habilitacao) = 'D' AND
+																			  UPPER(Categoria_Habilitacao) = 'E'),
 CategoriaID						INT						NOT NULL
 
 PRIMARY KEY(ID)
@@ -89,9 +110,9 @@ CREATE TABLE Reparo (
 	VeiculoPlaca			CHAR(7)					NOT NULL,
 	FuncionarioID			INT						NOT NULL,
 	PecaID					INT						NOT NULL,
-	Data_Reparo				DATE					NOT NULL,
-	Custo_Total				DECIMAL(4, 2)			NOT NULL,
-	Tempo					INTEGER					NOT NULL
+	Data_Reparo				DATE					NOT NULL		DEFAULT('15/10/2024'),
+	Custo_Total				DECIMAL(4, 2)			NOT NULL		CHECK(Custo_Total >= 0.00),
+	Tempo					INTEGER					NOT NULL		CHECK(Tempo >= 0)
 
 	PRIMARY KEY(Data_Reparo, VeiculoPlaca, FuncionarioID, PecaID)
 	
@@ -101,3 +122,12 @@ CREATE TABLE Reparo (
 
 )
 GO
+
+/*EXEC SP_HELP Cliente
+EXEC SP_HELP Veiculo
+EXEC SP_HELP Telefone_Cliente
+EXEC SP_HELP Peca
+EXEC SP_HELP Categoria
+EXEC SP_HELP Funcionario
+EXEC SP_HELP Reparo
+*/

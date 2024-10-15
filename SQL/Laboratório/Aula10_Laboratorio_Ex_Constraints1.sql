78b5099060b1b8c92957acb5a3a519d78254e251
Seraphin <149839735+KawanSerafim@users.noreply.github.com>
2024-10-15 19:04:09 -0300

    Update Aula10_Laboratorio_Ex_Constraints1.sql - Corrigido

--- SQL/Laboratório/Aula10_Laboratorio_Ex_Constraints1.sql
+++ SQL/Laboratório/Aula10_Laboratorio_Ex_Constraints1.sql
@@ -1,29 +1,8 @@
-/*
-RESTRIÇÕES DO EXERCÍCIO
-
-- O código do livro é auto incremental partindo de 100001 e incrementando de 100 em 100
-- O código do autor é auto incremental partindo de 2351 e incrementando de 1 em 1
-- O código da editora é auto incremental partindo de 491 e incrementando de 16 em 16
-- Por lei, o ISBN deve ter 13 dígitos
-- O nome do autor é único
-- O nome da editora é único
-- Se não preenchida, a língua dos livros é ‘PT-BR’
-- Não são cadastrados livros de ano inferior a 1990
-- Não são cadastrados livros de edição inferior a 1993
-- Não existem preços negativos
-- Não existem livros com menos de 15 páginas
-- Não existem Números de Endereços negativos
-- Telefones são fixos devem ter 10 dígitos
-- No Brasil, todo CEP tem 8 caracteres
-- Só são cadastrados livros de autores do Brasil, Estados Unidos, Inglaterra e Alemanha
-*/
-
-
 CREATE DATABASE livraria
 GO
 USE livraria
 GO
-	
+
 CREATE TABLE Livro (
 
 Codigo		INT					NOT NULL		IDENTITY(100001, 100),
@@ -104,4 +83,3 @@ FOREIGN KEY(EdicaoISBN) REFERENCES Edicao(ISBN),
 FOREIGN KEY(LivroCodigo) REFERENCES Livro(Codigo)
 
 )
-GO
